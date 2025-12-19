3127cce66aeeab646aaf85a378bcfb195468a8af
Brock Wyma <brockdwyma@gmail.com>
2025-12-19 13:57:59 -0600

    updated versions

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
 		.package(url:"https://github.com/tannerdsilva/QuickLMDB.git", "14.0.0"..<"15.0.0"),
 //		.package(path:"../nostr-kit-swift"),
 		.package(url:"https://github.com/bwyma1/nostr-kit-swift", revision:"a8072a1bee5bc7541a8b5375b1aa0e4b370665c4"),
-		.package(url:"https://github.com/tannerdsilva/wireguard-swift", revision:"f015dfe134763a1daa460c623b3c0b8bcb6fcd97"),
+		.package(url:"https://github.com/tannerdsilva/wireguard-swift", revision:"0eb24a67c30a5178526383ca5210b41f6b1e48c9"),
 //		.package(path:"../wireguard-swift"),
 
 		.package(url:"https://github.com/apple/swift-nio.git", "2.81.0"..<"3.0.0"),
